f9d32cb9cdc2813ed621c73ecc7174a4c3c26df1
zarakjhang-dev <zarakiftikhar510@gmail.com>
2024-12-08 20:43:10 +0200

    front end changes
    frontend changes

--- frontend/src/components/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
-
-const PrivateRoute = () => {
-  const { userInfo } = useSelector((state) => state.auth);
-  return userInfo ? <Outlet /> : <Navigate to="/pages/login" replace />;
-};
-export default PrivateRoute;
--- frontend/src/components/PublicRoute.js
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
-
-const PublicRoute = () => {
-  const { userInfo } = useSelector((state) => state.auth);
-  return userInfo ? <Navigate to="/pages/profile" replace /> : <Outlet />;
-};
-
-export default PublicRoute;
